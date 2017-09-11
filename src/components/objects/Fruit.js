f4fd596e1d73104b13f5c9a4990e0bdab2965f82
Gustaf Elbander <gelbander@gmail.com>
2017-09-11 08:52:47 +0200

    Load different fruit based on value.

--- src/components/objects/Fruit.js
+++ src/components/objects/Fruit.js
@@ -1,13 +1,19 @@
 import Phaser from 'phaser';
 
+const ASSET_VALUE_MAP = {
+    '1': 'fruit',
+    '2': 'banana',
+    '3': 'red'
+}
+
 class Fruit extends Phaser.Sprite {
 
     /**
      * Fruit constructor
      * @param {Phaser.Game} game the game by which to spawn the player
      */
-    constructor(game, x, y) {
-        super(game, x, y, 'fruit');
+    constructor(game, x, y, value) {
+        super(game, x, y, ASSET_VALUE_MAP[value]);
 
         this.width = game.settings.GRID_SIZE;
         this.height = game.settings.GRID_SIZE;
